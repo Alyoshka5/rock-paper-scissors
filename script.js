104d5d0523ac7215ce39e0cf83263655a3e40155
Alyoshka <alyoshkamyshko@gmail.com>
2022-03-18 18:30:00 -0700

    Create function game() and fix bug in function playRound()
    Function game() simulates 5 rounds of rock, paper, scissors. It asks the
    player for their choice and then calls the playRound() function, passing
    it the player's choice and the computer's randomized choice by calling
    function computerPlay(). After every round, the result of the round is
    displayed and the score of the winner is incremented. After five
    rounds, the result of the game is displayed.
    
    Bug in function playRound() caused any input that wasn't one of the three
    valid inputs to be displayed as a tie. Now if the player provides an
    invalid input, "Invalid Input!" will be displayed and no one scores for
    that round will be incremented.

--- script.js
+++ script.js
@@ -6,14 +6,33 @@ function computerPlay() {
 
 function playRound(player, computer) {
     // tie = 0; computer = 1; player = 2
-    if (player == "rock" && computer == "paper") return [1, "You Lose! Paper beats Rock"];
+    if (player == computer) return [0, `Tie! Both picked ${player}`];
+
+    else if (player == "rock" && computer == "paper") return [1, "You Lose! Paper beats Rock"];
     else if (player == "rock" && computer == "scissors") return [2, "You Win! Rock beats Scissors"];
     
     else if (player == "paper" && computer == "rock") return [2, "You Win! Paper beats Rock"];
     else if (player == "paper" && computer == "scissors") return [1, "You Lose! Scissors beats Paper"];
     
-    else if (player == "scissors" && computer == "rock") return [2, "You Lose! Rock beats Scissors"];
-    else if (player == "scissors" && computer == "paper") return [1, "You Win! Scissors beats Paper"];
+    else if (player == "scissors" && computer == "rock") return [1, "You Lose! Rock beats Scissors"];
+    else if (player == "scissors" && computer == "paper") return [2, "You Win! Scissors beats Paper"];
 
-    else return [0, `Tie! Both picked ${player}`];
+    else return [0, "Invalid Input!"];
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        const result = playRound(playerChoice, computerPlay());
+        if (result[0] == 1) computerScore++;
+        else if (result[0] == 2) playerScore++;
+        console.log(result[1]);
+    }
+    if (playerScore > computerScore) console.log(`You won the game!\nScore: ${playerScore}:${computerScore}`);
+    else if (computerScore > playerScore) console.log(`You lost the game!\nScore: ${playerScore}:${computerScore}`);
+    else console.log(`The game was a tie!\nScore: ${playerScore}:${computerScore}`);
+}
+
+game();
