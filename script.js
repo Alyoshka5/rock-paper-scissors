fea7bd2ede8f23fd7c410f3830c9e36fa008c83d
Alyoshka <alyoshkamyshko@gmail.com>
2022-03-18 18:11:19 -0700

    Create function playRound to simulate rock paper scissors playRound
    playRound receives as input the player's and computer's choice and
    returns an array. The first item in the array is an integer representing
    who won (0 = tie, 1 = computer, 2 = player) and the second item being
    the message that will be displayed to the player.

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ function computerPlay() {
     return plays[Math.floor((Math.random() * 3) + 1) - 1];
 }
 
-for (var i = 0; i < 10; i++) {console.log(computerPlay())}
+function playRound(player, computer) {
+    // tie = 0; computer = 1; player = 2
+    if (player == "rock" && computer == "paper") return [1, "You Lose! Paper beats Rock"];
+    else if (player == "rock" && computer == "scissors") return [2, "You Win! Rock beats Scissors"];
+    
+    else if (player == "paper" && computer == "rock") return [2, "You Win! Paper beats Rock"];
+    else if (player == "paper" && computer == "scissors") return [1, "You Lose! Scissors beats Paper"];
+    
+    else if (player == "scissors" && computer == "rock") return [2, "You Lose! Rock beats Scissors"];
+    else if (player == "scissors" && computer == "paper") return [1, "You Win! Scissors beats Paper"];
+
+    else return [0, `Tie! Both picked ${player}`];
+}
